d1ec12fc069635263dd8088b5c6cd025b7a92ffc
Makoy <neimarkbraga@gmail.com>
2017-09-10 23:27:17 +0800

    updated places page

--- public/app/pages/public/placesCtrl.js
+++ public/app/pages/public/placesCtrl.js
@@ -15,7 +15,10 @@ angular.module('publicCtrls')
                 if(err) throw err;
                 for(var i = 0; i < data.length; i++){
                     if(!places.district[data[i].DISTRICT_NAME]) places.district[data[i].DISTRICT_NAME] = [];
-                    places.district[data[i].DISTRICT_NAME].push(data[i].BARANGAY_NAME)
+                    places.district[data[i].DISTRICT_NAME].push({
+                        id: data[i].BARANGAY_ID,
+                        name: data[i].BARANGAY_NAME
+                    });
                 }
                 for(var district in places.district){
                     places.chart.labels.push(district);
